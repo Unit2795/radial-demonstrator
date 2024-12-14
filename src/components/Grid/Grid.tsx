1308480906497bb00fbdcaed5c3ccb7c619ac45f
David Jozwik <david@djoz.us>
2024-12-14 00:41:44 -0600

    Logic for creating spokes

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,4 +1,5 @@
 import {useEffect, useRef, useState} from "react";
+import {throttle} from "lodash";
 
 // Amount of padding in pixels to add to the edge of the SVG container, to prevent clipping
 const PADDING = 20;
@@ -6,9 +7,36 @@ const PADDING = 20;
 const Grid = () => {
 	const svgRef = useRef<SVGSVGElement>(null);
 
-	const [concentrics, setConcentrics] = useState(5);
-	const [containerSize, setContainerSize] = useState({ width: 0, height: 0, maxRadius: 0 });
+	const [radials] = useState(5);
+	const [spokes] = useState(12);
+	const [containerSize, setContainerSize] = useState({
+		width: 0,
+		height: 0,
+		maxRadius: 0,
+		center: {
+			x: 0,
+			y: 0
+		}
+	});
+	const {maxRadius, center} = containerSize;
 
+
+	const throttledResize = throttle((entries) => {
+		for (const entry of entries) {
+			const { width, height } = entry.contentRect;
+			// Calculate the maximum radius that can fit in the SVG container.
+			const maxRadius = Math.min(width, height) / 2 - PADDING;
+			setContainerSize({
+				width,
+				height,
+				maxRadius,
+				center: {
+					x: width / 2,
+					y: height / 2
+				}
+			});
+		}
+	}, 500);
 
 	// Observe resize events on the SVG container to update the graph as needed
 	useEffect(() => {
@@ -16,12 +44,7 @@ const Grid = () => {
 		if (!svgElement) return;
 
 		const resizeObserver = new ResizeObserver((entries) => {
-			for (const entry of entries) {
-				const { width, height } = entry.contentRect;
-				// Calculate the maximum radius that can fit in the SVG container.
-				const maxRadius = Math.min(width, height) / 2 - PADDING;
-				setContainerSize({ width, height, maxRadius });
-			}
+			throttledResize(entries);
 		});
 
 		resizeObserver.observe(svgElement);
@@ -39,14 +62,13 @@ const Grid = () => {
 			style={{
 				minHeight: "100vh",
 			}}
+			viewBox="0 0 100 100"
 			ref={svgRef}
-			width="100%"
-			height="100%"
 		>
 			{
-				Array.from({length: concentrics}).map((_, i) => {
+				Array.from({length: radials}).map((_, i) => {
 					// Calculate the radius for the current radial axis
-					const radius = (containerSize.maxRadius / concentrics) * (i + 1);
+					const radius = (maxRadius / radials) * (i + 1);
 
 					return (
 						<circle
@@ -56,6 +78,26 @@ const Grid = () => {
 							r={radius}
 							stroke="white"
 							fill="none"
+							strokeWidth="1"
+						/>
+					);
+				})
+			}
+			{
+				Array.from({length: spokes}).map((_, i) => {
+					const angle = (i * 2 * Math.PI) / spokes;
+					const x2 = center.x + Math.cos(angle) * maxRadius;
+					const y2 = center.y + Math.sin(angle) * maxRadius;
+
+					return (
+						<line
+							key={i}
+							x1={center.x}
+							y1={center.y}
+							x2={x2}
+							y2={y2}
+							stroke={"white"}
+							strokeWidth={1}
 						/>
 					);
 				})
