5e0648178d3e9803985c725ac9053fb223cfe0c7
David Jozwik <david@djoz.us>
2024-12-14 02:21:02 -0600

    Add tailwind, test out responsive SVG sizing based on viewport instead of container-defined pixels for easier resizing

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,74 +1,27 @@
-import {useEffect, useRef, useState} from "react";
-import {throttle} from "lodash";
+import {useRef, useState} from "react";
 
 // Amount of padding in pixels to add to the edge of the SVG container, to prevent clipping
 const PADDING = 20;
+const VIEWPORT = 1000;
+const CENTER = VIEWPORT / 2;
+const MAXRADIUS = VIEWPORT / 2 - PADDING;
 
 const Grid = () => {
 	const svgRef = useRef<SVGSVGElement>(null);
 
-	const [radials] = useState(5);
-	const [spokes] = useState(12);
-	const [containerSize, setContainerSize] = useState({
-		width: 0,
-		height: 0,
-		maxRadius: 0,
-		center: {
-			x: 0,
-			y: 0
-		}
-	});
-	const {maxRadius, center} = containerSize;
-
-
-	const throttledResize = throttle((entries) => {
-		for (const entry of entries) {
-			const { width, height } = entry.contentRect;
-			// Calculate the maximum radius that can fit in the SVG container.
-			const maxRadius = Math.min(width, height) / 2 - PADDING;
-			setContainerSize({
-				width,
-				height,
-				maxRadius,
-				center: {
-					x: width / 2,
-					y: height / 2
-				}
-			});
-		}
-	}, 500);
-
-	// Observe resize events on the SVG container to update the graph as needed
-	useEffect(() => {
-		const svgElement = svgRef.current;
-		if (!svgElement) return;
-
-		const resizeObserver = new ResizeObserver((entries) => {
-			throttledResize(entries);
-		});
-
-		resizeObserver.observe(svgElement);
-
-		// Cleanup observer on unmount
-		return () => {
-			if (!svgElement) return;
-			resizeObserver.unobserve(svgElement);
-			resizeObserver.disconnect();
-		};
-	}, []);
+	const [radials] = useState(6);
+	const [spokes] = useState(6);
 
     return (
 		<svg
-			style={{
-				minHeight: "100vh",
-			}}
-			viewBox="0 0 100 100"
+			viewBox={`0 0 ${VIEWPORT} ${VIEWPORT}`}
 			ref={svgRef}
+			preserveAspectRatio="xMidYMid meet"
 		>
 			{
 				Array.from({length: radials}).map((_, i) => {
 					// Calculate the radius for the current radial axis
-					const radius = (maxRadius / radials) * (i + 1);
+					const radius = (MAXRADIUS / radials) * (i + 1);
 
 					return (
 						<circle
@@ -86,14 +39,14 @@ const Grid = () => {
 			{
 				Array.from({length: spokes}).map((_, i) => {
 					const angle = (i * 2 * Math.PI) / spokes;
-					const x2 = center.x + Math.cos(angle) * maxRadius;
-					const y2 = center.y + Math.sin(angle) * maxRadius;
+					const x2 = CENTER + Math.cos(angle) * MAXRADIUS;
+					const y2 = CENTER + Math.sin(angle) * MAXRADIUS;
 
 					return (
 						<line
 							key={i}
-							x1={center.x}
-							y1={center.y}
+							x1={CENTER}
+							y1={CENTER}
 							x2={x2}
 							y2={y2}
 							stroke={"white"}
